9c3abece342717c1845cf505a11e6b16c78c9cb7
yue-os <calimbo657@gmail.com>
2025-07-14 14:48:29 +0800

    Update util.lua

--- util.lua
+++ util.lua
@@ -59,8 +59,8 @@ getgenv().gag = loadstring(game:HttpGet("https://raw.githubusercontent.com/yue-o
     }
 
     u.item_codes = {}
-    for k, v in pairs(item_types) do
-        item_codes[v] = k
+    for k, v in pairs(gag.item_types) do
+        gag.item_codes[v] = k
     end
 
 

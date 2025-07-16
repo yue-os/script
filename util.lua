da3eedd36a4d79829b28a8e1b9e3807fd4a81fae
yue-os <calimbo657@gmail.com>
2025-07-17 02:11:12 +0800

    Update util.lua

--- util.lua
+++ util.lua
@@ -62,7 +62,7 @@ end
 -- end
 
 function u.getBaseName(name)
-    name = name:gsub("%b[]", "")
+    name = name:gsub("%s*%b[]", "")
     name = name:gsub("%s[xX]%d+$", "")
     name = name:gsub("%sSeed$", "") -- Remove "Seed" only if at the end
     return name:match("^%s*(.-)%s*$")
